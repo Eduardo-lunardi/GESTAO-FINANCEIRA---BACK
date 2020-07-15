0586b0d4074d517158d3d162dafadfc419f09e9c
Eduardo <eduardolunardi942@gmail.com>
2020-07-14 22:14:30 -0300

    criacao do back e rota para login cad user

---
+++ index.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const
+    express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser'),
+    cors = require('cors'),
+    db = require('./services/Database'),
+
+    authVerify = require('./middlewares/authVerify'),
+    authRoutes = require('./routes/authRoutes'),
+    usuarioRoutes = require('./routes/usuarioRoutes')
+    
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var corsOptions = {
+    origin: '*',
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
+app.use(authRoutes)// rota de login
+app.use(authVerify) // validacao de token
+app.use('/usuario', usuarioRoutes)
+
+const port = process.env.SERVER_PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+});
